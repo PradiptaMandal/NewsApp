89071b534167fdfcb3ca8353739dc613775bb692
Pradipta Mandal <pradiptamandal789@gmail.com>
2023-03-27 20:17:01 +0530

    news.js files updated

--- src/components/News.js
+++ src/components/News.js
@@ -2,28 +2,47 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 
 export class News extends Component {
-    articles = [{"source":{"id":"espn-cric-info","name":"ESPN Cric Info"},"author":null,"title":"PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com","description":"Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com","url":"http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket","urlToImage":"https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg","publishedAt":"2020-04-27T11:41:47Z","content":"Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"},
-    {"source":{"id":"espn-cric-info","name":"ESPN Cric Info"},"author":null,"title":"What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com","description":"Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com","url":"http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again","urlToImage":"https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg","publishedAt":"2020-03-30T15:26:05Z","content":"Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"}]
+    articles = []
     constructor() {
         super();
         console.log("hello im a contructor")
         this.state = {
             articles: this.articles,
-            loading : false
+            loading : false,
+            page:1
         }
     }
     async componentDidMount() {
       console.log("cdm")
-      let url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=87038f89552b49449c9122a9ddce124f"
+      let url = "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=87038f89552b49449c9122a9ddce124f"
       let data = await fetch(url);
       let parsedData = await data.json()
       console.log(parsedData)
       this.setState({articles: parsedData.articles})
     }
+    handlePrevClick = async()=>{
+      console.log("prev clicked")
+    }
+    handleNextClick = async()=>{
+      console.log("Next");
+      if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+      }
+      else {
+          let url = `https://newsapi.org/v2/top-headlines?country=in&apikey=dbe57b028aeb41e285a226a94865f7a7&page=${this.state.page + 1}&pageSize=20`;
+          let data = await fetch(url);
+          let parsedData = await data.json()
+          console.log(parsedData);
+          this.setState({
+              page: this.state.page + 1,
+              articles: parsedData.articles
+          })
+      }
+
+    }
   render() {
     return (
       <div className="container my-3">
-        <h2>NewsMonkey Top Headlines</h2>
+        <h1 className="text-center">NewsMonkey Top Headlines</h1>
         
         <div className="row">
         {this.state.articles.map((element)=>{
@@ -34,6 +53,10 @@ export class News extends Component {
         })}
           
           
+        </div>
+        <div className="container d-flex justify-content-between">
+        <button disabled={this.state.page<=1} type="button" class="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
+        <button type="button" class="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
         </div>
       </div>
     );
